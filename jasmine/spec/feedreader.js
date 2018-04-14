72f9964a1ee7ea9fd4c6b57f15a55511157565b7
Moomba Muloba <moomba.muloba@cohaesus.co.uk>
2018-04-14 22:06:52 +0100

    Write tests for the RSS feeds and menu display

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,29 +31,58 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+        it('each have URL that is not empty', function() {
+            allFeeds.forEach(function(feed) {
+                expect(feed.url).toBeDefined();
+                expect(feed.url.length).not.toBe(0);
+            })
+        });
 
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it('each have a name that is not empty', function() {
+            allFeeds.forEach(function(feed) {
+                expect(feed.name).toBeDefined();
+                expect(feed.name.length).not.toBe(0);
+            });
+        });
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
-
+    describe('The menu', function() {    
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
-
+        it('should be hidden by default', function() {
+            // body element is hidden using the menu-hidden class
+            let myBody = $("body").hasClass("menu-hidden");
+            expect(myBody).toBe(true);
+        });
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+          it('can be displayed and hidden on click', function() {
+              // get body and menu icon element
+              let myBody = $("body");
+              let myMenuIcon = $('.menu-icon-link');
 
+              // We will test the click event in app.js which handles the menu toggle
+              // remove the menu class
+              myMenuIcon.trigger("click");
+              expect(myBody.hasClass("menu-hidden")).toBe(false);
+              // add the menu class
+              myMenuIcon.trigger("click");
+              expect(myBody.hasClass("menu-hidden")).toBe(true);
+          });
+    });
     /* TODO: Write a new test suite named "Initial Entries" */
 
         /* TODO: Write a test that ensures when the loadFeed
